9baa4f3d12cb00565ba50c34b3e2706d53c254dc
lippirk <ben.anson@citrix.com>
2020-07-06 16:08:31 +0100

    tmp

--- .travis-xs-opam.sh
+++ .travis-xs-opam.sh
@@ -6,7 +6,7 @@ PACKAGE="xapi"
 PINS="xapi:. xapi-cli-protocol:. xapi-client:. xapi-consts:. xapi-database:. xapi-datamodel:. xapi-types:. xe:."
 
 wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-docker.sh
-wget https://raw.githubusercontent.com/xapi-project/xs-opam/release/stockholm/lcm/tools/xs-opam-ci.env
+wget https://raw.githubusercontent.com/lippirk/xs-opam/release/stockholm/lcm/tools/xs-opam-ci.env
 . xs-opam-ci.env
 . .travis-docker.sh
 
