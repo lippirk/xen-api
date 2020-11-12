afdb1ac7c47277158cd304f3cf186b7a934e126e
lippirk <ben.anson@citrix.com>
2020-11-12 12:17:01 +0000

    REQ-403 fqdn of hostname
    Signed-off-by: lippirk <ben.anson@citrix.com>

--- ocaml/gencert/lib.mli
+++ ocaml/gencert/lib.mli
@@ -14,6 +14,8 @@
 
 val get_management_ip_addr : dbg:string -> string option
 
+val fqdn_of_hostname : string -> string option
+
 val hostnames : unit -> string list
 (** Try to get all FQDNs, use the hostname if none are available *)
 
